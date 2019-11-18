71161a653fbd01153220501d94c0f4ecffe043a6
Juliette Keller <kellerjuliette@hotmail.com>
2019-11-18 12:32:38 +0000

    adding flat model

---
+++ db/migrate/20191118122725_create_flats.rb
@@ -0,0 +1,14 @@
+class CreateFlats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flats do |t|
+      t.string :name
+      t.integer :price_per_night
+      t.integer :capacity
+      t.integer :rating
+      t.string :location
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
