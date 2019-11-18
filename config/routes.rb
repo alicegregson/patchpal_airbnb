e7884a671f672ecc131581a6f8f4a2fa6ee7fd43
Alice Gregson <gregsonalice1@gmail.com>
2019-11-18 16:50:02 +0000

    added review model and updated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/destroy'
-
-  get 'flats/index'
-  get 'flats/show'
-  get 'flats/new'
-  get 'flats/create'
-  get 'flats/edit'
-  get 'flats/update'
-  get 'flats/destroy'
-
   devise_for :users
-  root to: 'pages#home'
+  root to: 'flats#index'
+  resources :flats do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:show, :edit, :update, :index, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
