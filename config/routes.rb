b53990c8579d1d92e396ebdbf4e912b1b4027ad5
Alice Gregson <gregsonalice1@gmail.com>
2019-11-18 15:02:37 +0000

    routes update 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'flats#index'
   resources :flats, except: :index do
-    resources :bookings, only: [:new, :create, :index, :show]
+    resources :bookings, only: [:new, :create, :index, :show, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
