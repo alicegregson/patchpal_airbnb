6d6d780741bb9ea28c253d927245559fc4c6b4c6
Olly Taylor <ollyjtaylor@live.co.uk>
2019-11-20 11:11:50 +0000

    added mapbox

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,7 +1,14 @@
 class FlatsController < ApplicationController
   def index
     @home = true
-    @flats = Flat.all
+    @flats = Flat.where.not(latitude: nil, longitude: nil)
+
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def show
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -6,4 +6,6 @@ class Flat < ApplicationRecord
   validates :price_per_night, presence: true
   validates :name, presence: true, uniqueness: true
   validates :capacity, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
