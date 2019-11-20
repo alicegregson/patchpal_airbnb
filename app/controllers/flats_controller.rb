a6dc792fddd6fa70c0179769d6c62260efdbda7d
Alice Gregson <gregsonalice1@gmail.com>
2019-11-20 14:50:39 +0000

    added map

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -16,6 +16,13 @@ class FlatsController < ApplicationController
 
   def show
     @flat = Flat.find(params[:id])
+     # @flat = Flat.geocoded
+    #  @marker = @flat.map do |flat|
+    #   {
+    #     lat: flat.latitude,
+    #     lng: flat.longitude
+    #   }
+    # end
   end
 
   def new
