efafdb2fd5fcbbef2a27e535e4355b92e253d1fb
Juliette Keller <kellerjuliette@hotmail.com>
2019-11-21 16:57:05 +0000

    btns again

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -17,7 +17,7 @@ class FlatsController < ApplicationController
       {
         lat: flat.latitude,
         lng: flat.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat }),
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat })
         # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
       }
     end
