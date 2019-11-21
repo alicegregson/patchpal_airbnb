11c90886108185e80a4ba215931045d31fac84ec
Alice Gregson <gregsonalice1@gmail.com>
2019-11-21 14:18:29 +0000

    updated pundit for create

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -42,12 +42,13 @@ class FlatsController < ApplicationController
 
   def edit
     @flat = Flat.find(params[:id])
+    authorize @flat
   end
 
   def update
     @flat = Flat.find(params[:id])
-    @flat.update(flat_params)
     authorize @flat
+    @flat.update(flat_params)
     redirect_to flat_path(@flat)
   end
 
