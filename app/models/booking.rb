1dd68341073d67b2798599883fb828ca6c212ce6
Alice Gregson <gregsonalice1@gmail.com>
2019-11-21 11:23:03 +0000

    created booking page with review form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,5 +5,5 @@ class Booking < ApplicationRecord
   validates  :check_in, presence: true
   validates  :check_out, presence: true
   validates  :price, presence: true
-  validates  :number_of_guests, presence: true
+  validates  :number_of_guest, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-flat1 = Flat.create(name: 'Hoxton 100', location: 'Shoreditch', price_per_night: 345, capacity: 5, rating: 4)
-flat2 = Flat.create(name: 'Bougie House', location: 'Shoreditch', price_per_night: 345, capacity: 5, rating: 4)
-flat3 = Flat.create(name: "Jana's Cozy Coop", location: 'Shoreditch', price_per_night: 345, capacity: 5, rating: 4)
+# flat1 = Flat.create(name: 'Hoxton 100', location: 'Shoreditch', price_per_night: 345, capacity: 5, rating: 4)
+# flat2 = Flat.create(name: 'Bougie House', location: 'Shoreditch', price_per_night: 345, capacity: 5, rating: 4)
+# flat3 = Flat.create(name: "Jana's Cozy Coop", location: 'Shoreditch', price_per_night: 345, capacity: 5, rating: 4)
+
+booking1 = Booking.create(check_in: "08/09/2020", check_out: "09/09/2020", price: 500, number_of_guest: 3, flat_id: 3, user_id: 1)
+
+
+ # t.date "check_in"
+ #    t.date "check_out"
+ #    t.integer "price"
+ #    t.integer "number_of_guest"
+ #    t.bigint "flat_id"
+ #    t.bigint "user_id"
